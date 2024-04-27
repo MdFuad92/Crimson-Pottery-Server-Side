ed1c6cdb3d7b137b684cbe5f25320ce7261ff41e
Mdfuad92 <mdfuad98@gmail.com>
2024-04-27 22:26:48 +0600

    added new data to the database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -29,6 +29,52 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const craftCollection = client.db('craftDB').collection('crafts')
+    const userCollection = client.db('craftDB').collection('users')
+   
+
+    app.post('/crafts',async(req,res)=>{
+      const craft = req.body
+      const result = await craftCollection.insertOne(craft)
+      res.send(result)
+    })
+
+    app.get('/crafts',async(req,res)=>{
+        const cursor = craftCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    app.get('/crafts/:id',async(req,res)=>{
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const result = await craftCollection.findOne(query)
+      res.send(result)
+    })
+    app.get('/crafts/:email',async(req,res)=>{
+      const name = req.params.email 
+      console.log(name)
+      const cursor = {email:email}
+      const result = await craftCollection.find(cursor).toArray()
+      res.send(result)
+      
+    })
+    
+
+    //users database
+    app.post('/users',async(req,res)=>{
+        const user = req.body
+        console.log(user)
+        const result = await userCollection.insertOne(user)
+        res.send(result)
+    })
+
+    app.get('/users',async(req,res)=>{
+        const cursor = userCollection.find()
+        const users = await cursor.toArray()
+        res.send(users)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
