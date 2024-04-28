47b317a61e83fce21b2deefd750256f787d5d7c5
Mdfuad92 <mdfuad98@gmail.com>
2024-04-29 03:27:40 +0600

    added a delete route

--- index.js
+++ index.js
@@ -90,12 +90,7 @@ async function run() {
       res.send(result)
     })
     
-    app.delete('/crafts/id/:id',async(req,res)=>{
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-      const result = await craftCollection.deleteOne(query)
-      res.send(result)
-    })
+  
 
 
 
